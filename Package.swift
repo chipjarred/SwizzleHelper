ae7801a0a521324078523bef169076ff2f6cb318
Chip Jarred <chipjarred@gmail.com>
2021-04-06 08:27:48 -0500

    Checking in SwizzleHelper sources

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwizzleHelper",
+    platforms: [.macOS(.v10_14), .iOS(.v12), .tvOS(.v12), .watchOS(.v5)],
+    products:
+    [
+        .library(
+            name: "SwizzleHelper",
+            targets: ["SwizzleHelper", "SwizzleHelperObjC"]
+        ),
+    ],
+    dependencies: [],
+    targets:
+    [
+        .target(
+            name: "SwizzleHelper",
+            dependencies: ["SwizzleHelperObjC"]
+        ),
+        .target(
+            name: "SwizzleHelperObjC",
+            dependencies: [],
+            path: "SwizzleHelperObjC",
+            cSettings: [
+               .headerSearchPath("Internal"), 
+            ]
+        ),
+        .testTarget(
+            name: "SwizzleHelperTests",
+            dependencies: ["SwizzleHelper"]
+        ),
+    ]
+)
